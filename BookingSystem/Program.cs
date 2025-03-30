bb6148636ccc0103ec93ff20897ffe10eac5f309
Niklas H <156240330+mymh13@users.noreply.github.com>
2025-03-30 22:19:44 +0200

    clarification on a comment

--- BookingSystem/Program.cs
+++ BookingSystem/Program.cs
@@ -78,7 +78,7 @@ await DatabaseSeeder.SeedData(app.Services);
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // Default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
